21b97ec686a611a4d42d0163e507aac96135a03f
Tolu Adesina <tadesina90@gmail.com>
2020-02-28 18:05:25 +0100

    update add products page with input and text area

--- src/Views/Products/add.js
+++ src/Views/Products/add.js
@@ -1,9 +1,37 @@
-import React from 'react';
+import React from "react";
+import Card from "../../Components/Card";
+
+import './styles.scss';
+import Input from "../../Components/Input";
+import TextArea from "../../Components/TextArea";
 
 export default props => {
   return (
-    <div className="product-section">
-      <h2>This is where we add products</h2>
+    <div className="add-product-section">
+      <div className="container">
+        <h2>Add products</h2>
+        <div className="row">
+          <div className="col _big">
+            <form className="form">
+              <Card>
+                <div className="form-group">
+                  <label>Title</label>
+                  <Input type="text" placeholder="Product Name" />
+                </div>
+                <div className="form-group">
+                  <label>Description</label>
+                  <TextArea placeholder="Describe your product" />
+                </div>
+              </Card>
+            </form>
+          </div>
+          <div className="col _small">
+            <Card>
+
+            </Card>
+          </div>
+        </div>
+      </div>
     </div>
   );
-}
+};
