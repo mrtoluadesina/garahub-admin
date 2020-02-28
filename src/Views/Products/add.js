6a5096ae33a7ac8f406381321e7f4c7da7ed7850
Tolu Adesina <tadesina90@gmail.com>
2020-02-28 18:53:23 +0100

    wrap add product markup with form element

--- src/Views/Products/add.js
+++ src/Views/Products/add.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Card from "../../Components/Card";
 
-import './styles.scss';
+import "./styles.scss";
 import Input from "../../Components/Input";
 import TextArea from "../../Components/TextArea";
 
@@ -10,27 +10,51 @@ export default props => {
     <div className="add-product-section">
       <div className="container">
         <h2>Add products</h2>
-        <div className="row">
-          <div className="col _big">
-            <form className="form">
+        <form className="form">
+          <div className="row">
+            <div className="col _big">
               <Card>
                 <div className="form-group">
-                  <label>Title</label>
+                  <h3>Title</h3>
                   <Input type="text" placeholder="Product Name" />
                 </div>
                 <div className="form-group">
-                  <label>Description</label>
+                  <h3>Description</h3>
                   <TextArea placeholder="Describe your product" />
                 </div>
               </Card>
-            </form>
+              <Card>
+                <div className="form-group">
+                  <h3>Images</h3>
+                  <Input type="file" />
+                </div>
+              </Card>
+              <Card>
+                <h3>Pricing</h3>
+                <div className="row">
+                  <div className="form-group">
+                    <label>Price</label>
+                    <Input type="number" placeholder="0.00" />
+                  </div>
+                  <div className="form-group">
+                    <label>Sales Price</label>
+                    <Input type="number" placeholder="0.00" />
+                  </div>
+                </div>
+              </Card>
+              <Card>
+                <h3>Inventory</h3>
+                <div className="form-group">
+                  <label>Stock</label>
+                  <Input type="number" placeholder="0" />
+                </div>
+              </Card>
+            </div>
+            <div className="col _small">
+              <Card></Card>
+            </div>
           </div>
-          <div className="col _small">
-            <Card>
-
-            </Card>
-          </div>
-        </div>
+        </form>
       </div>
     </div>
   );
