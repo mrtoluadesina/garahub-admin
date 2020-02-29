2f698dd7f4c5f7bbfd96d410904a0772382badc9
chidiogo-akunne <chidiogoakunne@gmail.com>
2020-02-29 14:20:39 +0100

    create product header

--- src/Views/Products/index.js
+++ src/Views/Products/index.js
@@ -1,11 +1,40 @@
-import React from 'react';
+import React from "react";
+import Icon from "@mdi/react";
 
-import './styles.scss';
+import { mdiChevronLeft, mdiChevronDown } from "@mdi/js";
+
+import OrderButton from "../../Components/OrderButton/index";
+import Dropdown from "../../Components/Dropdown/index";
+import Card from "../../Components/Card";
+import Table from "../../Components/Table";
+import Input from "../../Components/Input";
+import TableData from "../../utils/tabledata";
+import ProductTab from "../../Components/AbandonedTab";
+
+import "./styles.scss";
 
 export default props => {
   return (
-    <div className="products-section">
-      <h2>Products</h2>
+    <div className="product-row">
+      <div className="container">
+        <div className="product-header">
+          <h4 className="product">Products</h4>
+          <div className="productbtn">
+            <OrderButton value="Export" />
+          </div>
+        </div>
+        <ul className="product-ul">
+          <li className="export">Export</li>
+          <li className="export">Import</li>
+          <li>
+            <Dropdown>
+              <option>More Actions</option>
+              <option>Open</option>
+            </Dropdown>
+          </li>
+        </ul>
+        
+      </div>
     </div>
   );
 };
