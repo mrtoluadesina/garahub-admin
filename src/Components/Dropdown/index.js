19f6163dfb122037588acbe7b92e69b609fc1d0d
Tolu Adesina <tadesina90@gmail.com>
2020-02-29 12:42:29 +0100

    update dropdown component to remove unnecessary nested divs

--- src/Components/Dropdown/index.js
+++ src/Components/Dropdown/index.js
@@ -2,10 +2,4 @@ import React from 'react';
 
 import './index.scss';
 
-export default props => {
-  return (
-    <div>
-      <select>{props.children}</select>
-    </div>
-  );
-};
+export default props => <select>{props.children}</select>
