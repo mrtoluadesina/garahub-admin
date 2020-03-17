379a8d7f5cde64ff8b274ce4029deb48770c3065
oluwasayo <oluwasayoakinkunmi@gmail.com>
2020-03-17 14:49:37 +0100

    Implemented charts for analytics

--- src/Views/Analytics/index.js
+++ src/Views/Analytics/index.js
@@ -1,37 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./styles.scss";
 import DataCard from "../../Components/DataCard";
 import MasonryLayout from "../../Components/MasonryLayout";
+import axios from "axios";
 
 export default props => {
-  return (
-    <div className="admin-row">
-      <div className="container">
-        <div className="admin-header">
-          <h2 className="title">Overview dashboard</h2>
+    const [state, setState] = useState({
+        // data: {},
+        chartData: [
+            "revenue", 'order', 'revenue'
+        ]
+    });
+
+    return (
+        <div className="admin-row">
+            <div className="container">
+                <div className="admin-header">
+                    <h2 className="title">Overview dashboard</h2>
+                </div>
+                <div className="filter-data"></div>
+                <div className="content">
+                    <MasonryLayout>
+                        {state.chartData.map((item, index) => {
+                            return (<DataCard 
+                            data={item} 
+                            key={index} 
+                            statType={item}
+                            title={item.toUpperCase()}
+                            backgroundColor={''}
+                            label={'Date'}
+                            />);
+                        })}
+                    </MasonryLayout>
+                </div>
+            </div>
         </div>
-        <div className="filter-data"></div>
-        <div className="content">
-          <MasonryLayout>
-            <DataCard data={{ title: "Sales", data: { date: "25/02/2020" } }} />
-            <DataCard data={{ title: "Sales", data: { date: "25/02/2020" } }}>
-              <div className="sub-section">
-                <span>Visitors</span>
-                <span>0</span>
-              </div>
-            </DataCard>
-            <DataCard data={{ title: "Sales", data: { date: "25/02/2020" } }} />
-            <DataCard data={{ title: "Sales", data: { date: "25/02/2020" } }} />
-            <DataCard data={{ title: "Sales", data: { date: "25/02/2020" } }} />
-            <DataCard data={{ title: "Sales", data: { date: "25/02/2020" } }}>
-              <div className="sub-section">
-                <span>Visitors</span>
-                <span>0</span>
-              </div>
-            </DataCard>
-          </MasonryLayout>
-        </div>
-      </div>
-    </div>
-  );
+    );
 };
+
