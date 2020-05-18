d137ab3fad22f5efbe09fdcfadc459d7e362640a
Tolu Adesina <tadesina90@gmail.com>
2020-05-18 21:40:19 +0100

    remove unused useState declaration

--- src/Views/Transactions/index.js
+++ src/Views/Transactions/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 
 import OrderButton from "../../Components/OrderButton/index";
 import Table from "../../Components/Table";
