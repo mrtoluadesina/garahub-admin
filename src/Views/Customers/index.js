f4e6786ea5b95a8072c495b2555b4f20ed051ced
Tolu Adesina <tadesina90@gmail.com>
2020-05-19 02:01:52 +0100

    remove unnecessary elements on customer table

--- src/Views/Customers/index.js
+++ src/Views/Customers/index.js
@@ -1,11 +1,7 @@
 import React, {useEffect} from "react";
 
-import OrderButton from "../../Components/OrderButton/index";
-import Dropdown from "../../Components/Dropdown/index";
 import Card from "../../Components/Card";
 import Table from "../../Components/Table";
-import Input from "../../Components/Input";
-import CustomersTab from "../../Components/CustomerTab";
 
 import "./styles.scss";
 
@@ -28,57 +24,32 @@ export default props => {
         <div className="customer-header">
           <h4 className="customer">Customers</h4>
           <div className="customerbtn">
-            <OrderButton value="Add customer" />
+            {/* <OrderButton value="Add customer" /> */}
           </div>
         </div>
-        <ul className="customer-ul">
-            <li className="export">Import customers</li>
-            <li className="export">Export</li>
-            <li>
-            <Dropdown>
-              <option>More Actions</option>
-              <option>Open</option>
-            </Dropdown>
-          </li>
-        </ul>
         <Card className="customer-card">
           <div className="all-customer">
-            <CustomersTab />
-          </div>
-          <div className="filter">
-            <div className="filter-status">
-              <Dropdown>
-                <option>Filter</option>
-              </Dropdown>
-            </div>
-            <Input placeholder="Search customers"
-              className="customer-filterbar" />
-            <p className="sortBy">Sort by</p>
-            <div className="update-status">
-              <Dropdown>
-                <option>Newest update</option>
-              </Dropdown>
-            </div>
+            <span>Showing {customers.length} customers</span>
           </div>
           <Table>
               <thead className="th-color">
                 <tr>
-                  <th className="checkbox" scope="col">
-                    <input type="checkbox"></input>
-                  </th>
-                  <th scope="col">Showing {customers.length} customers</th>
-                  <th scope="col"></th>
-                  <th scope="col"></th>
+                  <th scope="col">S/N</th>
+                  <th scope="col">Name</th>
+                  <th scope="col">Email</th>
+                  <th scope="col">Phone</th>
+                  <th scope="col">Status</th>
                 </tr>
               </thead>
               <tbody>
                 {customers.map((item, index) => (
                   <tr key={index}>
-                    <td className="checkbox">
-                      <input type="checkbox"></input>
-                    </td>
+                    <td>{index + 1}</td>
                     <td>
                       <span className="color-dgray customer-padding">{item.firstName} {item.lastName}</span>
+                    </td>
+                    <td>
+
                       <span className="color-lgray"> {item.email}</span>
                     </td>
                     <td className="color-lgray">{item.phone}</td>
