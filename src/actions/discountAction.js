7edb16be77523c8f1bed3a60db069df6ee4995a2
Tolu Adesina <tadesina90@gmail.com>
2020-05-19 02:15:10 +0100

    create the dicount fetch action

---
+++ src/actions/discountAction.js
@@ -0,0 +1,38 @@
+import {
+  FETCH_DISCOUNT_END,
+  FETCH_DISCOUNT_FAIL,
+  FETCH_DISCOUNT_START,
+  FETCH_DISCOUNT_SUCCESS,
+} from "./types";
+import request from "../request";
+import { retrieveMessage } from "../utils/helperFunc";
+const fetchDiscountStart = payload => ({
+  type: FETCH_DISCOUNT_START,
+  payload
+});
+const fetchDiscountSuccess = payload => ({
+  type: FETCH_DISCOUNT_SUCCESS,
+  payload
+});
+const fetchDiscountFail = payload => ({
+  type: FETCH_DISCOUNT_FAIL,
+  payload
+});
+const fetchDiscountEnd = payload => ({
+  type: FETCH_DISCOUNT_END,
+  payload
+});
+
+export const fetchDiscounts = () => {
+  return async dispatch => {
+    try {
+      dispatch(fetchDiscountStart(true));
+      const res = await request.get("/api/v1/transaction");
+      dispatch(fetchDiscountSuccess(res.data.payload));
+      dispatch(fetchDiscountEnd(false));
+    } catch (error) {
+      dispatch(fetchDiscountFail(retrieveMessage(error)));
+      dispatch(fetchDiscountEnd(false));
+    }
+  };
+};
