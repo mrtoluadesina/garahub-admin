892cbe5761e1f6d46343ab4a0423193ba0d17082
Tolu Adesina <tadesina90@gmail.com>
2020-05-19 22:47:10 +0100

    add information cards to the dashboard

--- src/Views/Analytics/index.js
+++ src/Views/Analytics/index.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import "./styles.scss";
 import DataCard from "../../Components/DataCard";
 import MasonryLayout from "../../Components/MasonryLayout";
+import Card from "../../Components/Card";
 
 export default props => {
     const [state, setState] = useState({
@@ -17,7 +18,24 @@ export default props => {
                 <div className="admin-header">
                     <h2 className="title">Overview dashboard</h2>
                 </div>
-                <div className="filter-data"></div>
+                <div className="filter-data">
+                    <Card className="metrics-card">
+                        <h3>300</h3>
+                        <p>Products</p>
+                    </Card>
+                    <Card className="metrics-card">
+                        <h3>50</h3>
+                        <p>Orders</p>
+                    </Card>
+                    <Card className="metrics-card">
+                        <h3>30</h3>
+                        <p>Discounts</p>
+                    </Card>
+                    <Card className="metrics-card">
+                        <h3>27</h3>
+                        <p>Customers</p>
+                    </Card>
+                </div>
                 <div className="content">
                     <MasonryLayout>
                         {state.chartData.map((item, index) => {
