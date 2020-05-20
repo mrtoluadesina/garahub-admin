168566c5c2cea147fcdb09fdd43df7d3c9558e8c
jagajastic <josephibrahi@gmail.com>
2020-05-20 10:47:08 +0100

    add product functionality

--- src/Views/Products/add.js
+++ src/Views/Products/add.js
@@ -8,30 +8,51 @@ import Button from "../../Components/Button";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchCategories } from "../../actions/categoryActions";
 import Select from "react-select";
-import { uploadImage, priceArray,imagesPrepared, pricePrepared } from "../../utils/helperFunc";
-import { createPoductActions, updatePoductActions } from "../../actions/productAction";
+import {
+  uploadImage,
+  priceArray,
+  imagesPrepared,
+  pricePrepared,
+} from "../../utils/helperFunc";
+import {
+  createPoductActions,
+  updatePoductActions,
+} from "../../actions/productAction";
 import { BeatLoader } from "react-spinners";
 import izitoast from "izitoast";
-export default props => {
-  const { method, object={categoryId:[]} } = props;
-  const [uploadSuccess,setSuccess] = useState(false)
-  const [updateError,setUpdateError] = useState(false)
+import { fetchAllBrands } from "../../actions/brandsAction";
+export default (props) => {
+  const { method, object = { categoryId: [] } } = props;
+  const [uploadSuccess, setSuccess] = useState(false);
+  const [updateError, setUpdateError] = useState(false);
+
+  const brandStructure = {
+    value: "_id",
+    label: "name",
+  };
 
   const dispatch = useDispatch();
   const {
     category: { categories },
-    products: { success, error, productObj, fetching,updated }
-  } = useSelector(state => state);
+    products: { success, error, productObj, fetching, updated },
+    brands: { brands },
+  } = useSelector((state) => state);
 
+  console.log("add brand heree", brands);
+  const latestBrands = brands.map((brand) => {
+    return { ...brand, label: brand.name, value: brand._id };
+  });
   let [pricingCount, setPricingCount] = useState(1);
 
-  let [listOfPrices] = useState(method==="edit"?priceArray(object.price):[])
+  let [listOfPrices] = useState(
+    method === "edit" ? priceArray(object.price) : []
+  );
 
   let [pricingList, setPricingList] = useState([]);
 
   let [range, setRange] = useState({
     from: 0,
-    to: 0
+    to: 0,
   });
   const generateSku = () => "sku" + Date.now();
 
@@ -39,56 +60,56 @@ export default props => {
     name: "",
     description: "",
     quantity: 0,
-    categories: object?[...categories.filter(cat => {
-      
-      return object.categoryId.includes(cat.value)
-    })]:[],
-    sku: generateSku()
+    categories: object
+      ? [
+          ...categories.filter((cat) => {
+            return object.categoryId.includes(cat.value);
+          }),
+        ]
+      : [],
+    sku: generateSku(),
+    // brandId:""
   });
 
   const handleChange = ({ target }) => {
     createProduct({ ...product, [target.name]: target.value });
   };
- 
+
   let [images, createImages] = useState({});
 
   // let listOfArray=[]
 
   useEffect(() => {
-    
     dispatch(fetchCategories());
+    dispatch(fetchAllBrands());
     if (method === "edit") {
-      let array = []
-      object.categoryId.map((element,index)=>{
-        const cate=categories.filter((category,index)=>{
-          if(category.value===element){
-            array.push(categories[index])
+      let array = [];
+      object.categoryId.map((element, index) => {
+        const cate = categories.filter((category, index) => {
+          if (category.value === element) {
+            array.push(categories[index]);
             //setListOfArray(()=> [ ...listOfArray,categories[index]])
-            return index
-            
+            return index;
           }
-          
-        })
-        return cate
-      })
+        });
+        return cate;
+      });
       createProduct({
         ...product,
         name: object.name,
         description: object.description,
         quantity: object.quantity,
         categories: [...array],
-        price:object.price,
-        sku: object.sku
+        price: object.price,
+        sku: object.sku,
       });
-      setPricingCount(Object.keys(object.price).length)
-      const editImages = imagesPrepared(object.images)      
-      createImages({...images, ...editImages})
+      setPricingCount(Object.keys(object.price).length);
+      const editImages = imagesPrepared(object.images);
+      createImages({ ...images, ...editImages });
     }
   }, [success]);
 
   const handlePricingChange = ({ target }) => {
-    
-    
     let name = target.name;
     let index = name[name.length - 1];
 
@@ -100,8 +121,6 @@ export default props => {
       setRange({ ...range, [name]: target.value });
     }
     if (name.slice(0, name.length - 1) === "price") {
-      
-      
       setPricingList({ ...pricingList, [key]: target.value });
     }
   };
@@ -110,110 +129,112 @@ export default props => {
     createImages({ ...images, [target.name]: target.files[0] });
   };
 
-  const handleSelectChange = value => {
+  const handleSelectChange = (value) => {
     if (value) {
-      
       createProduct({ ...product, categories: [...value] });
       return;
     }
     createProduct({ ...product, categories: [] });
   };
-  const handleSubmit = async e => {
+  const handleBrandSelectChange = (value) => {
+    createProduct((prev) => ({ ...prev, brandId: value._id }));
+    return;
+  };
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    
-    
-    let imagesArray = []
-    let editPricing ={}
-    if (method==="edit"){
-      const refinedList =Object.values(images).filter(element=>!object.images.includes(element))
+
+    let imagesArray = [];
+    let editPricing = {};
+    if (method === "edit") {
+      const refinedList = Object.values(images).filter(
+        (element) => !object.images.includes(element)
+      );
       imagesArray = await uploadImage(Object.values(refinedList));
-      editPricing = pricePrepared(listOfPrices)
-      setSuccess(updated)
-      setUpdateError(error)
-    }
-    else {
+      editPricing = pricePrepared(listOfPrices);
+      setSuccess(updated);
+      setUpdateError(error);
+    } else {
       imagesArray = await uploadImage(Object.values(images));
-      setSuccess(success)
-      setUpdateError(error)
+      setSuccess(success);
+      setUpdateError(error);
     }
 
-    let categoryId = product.categories.map(category => category.value);
+    let categoryId = product.categories.map((category) => category.value);
     const data = {
       name: product.name,
       description: product.description,
       quantity: parseInt(product.quantity),
       sku: product.sku,
-      price: Object.keys(editPricing).length>0?{...editPricing}:{ ...pricingList },
-      images: imagesArray.length>0?[...imagesArray][0]:[Object.values(images)],
+      price:
+        Object.keys(editPricing).length > 0
+          ? { ...editPricing }
+          : { ...pricingList },
+      images:
+        imagesArray.length > 0 ? [...imagesArray][0] : [Object.values(images)],
       categoryId,
-    };   
-    // 
-    if (method!=="edit"){
+      brandId: product.brandId,
+    };
+    //
+    if (method !== "edit") {
       dispatch(createPoductActions(data));
+      setSuccess(success);
+    } else {
+      dispatch(updatePoductActions(object._id, data));
     }
-    else{
-      dispatch(updatePoductActions(object._id,data))
-    }
-    
-    
   };
-const handleEditPricingChange = ({target})=>{
-  let name = target.name
-  let index = name[name.length - 1];
-  let key =name.slice(0, name.length - 1)
-  if (name.slice(0, name.length - 1) === "from") {
-    setRange({ ...range, [name]: target.value });
-  }
-  if (name.slice(0, name.length - 1) === "to") {
-    setRange({ ...range, [name]: target.value });
-  }
-  if (name.slice(0, name.length - 1) === "price") {
-    setRange({ ...range, [name]: target.value });
-  }
-  listOfPrices[index][key] = target.value
-  
-  
-}
-  const priceEditGenerator=()=>{
-    return listOfPrices.map((list,index)=>(
+  const handleEditPricingChange = ({ target }) => {
+    let name = target.name;
+    let index = name[name.length - 1];
+    let key = name.slice(0, name.length - 1);
+    if (name.slice(0, name.length - 1) === "from") {
+      setRange({ ...range, [name]: target.value });
+    }
+    if (name.slice(0, name.length - 1) === "to") {
+      setRange({ ...range, [name]: target.value });
+    }
+    if (name.slice(0, name.length - 1) === "price") {
+      setRange({ ...range, [name]: target.value });
+    }
+    listOfPrices[index][key] = target.value;
+  };
+  const priceEditGenerator = () => {
+    return listOfPrices.map((list, index) => (
       <div className="d-flex">
         <div className="form-group">
-            <label htmlFor={`from${index}`}>From</label>
-            <Input
-              type="number"
-              name={`from${index}`}
-              onChange={handleEditPricingChange}
-              value={listOfPrices[index].from}
-            />
-          </div>
+          <label htmlFor={`from${index}`}>From</label>
+          <Input
+            type="number"
+            name={`from${index}`}
+            onChange={handleEditPricingChange}
+            value={listOfPrices[index].from}
+          />
+        </div>
         <div className="form-group">
-            <label htmlFor={`to${index}`}>To</label>
-            <Input
-              type="number"
-              name={`to${index}`}
-              onChange={handleEditPricingChange}
-              value={listOfPrices[index].to}
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor={`price${index}`}>Price</label>
-            <Input
-              type="number"
-              name={`price${index}`}
-              onChange={handleEditPricingChange}
-              value={listOfPrices[index].price}
-            />
-          </div>
+          <label htmlFor={`to${index}`}>To</label>
+          <Input
+            type="number"
+            name={`to${index}`}
+            onChange={handleEditPricingChange}
+            value={listOfPrices[index].to}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor={`price${index}`}>Price</label>
+          <Input
+            type="number"
+            name={`price${index}`}
+            onChange={handleEditPricingChange}
+            value={listOfPrices[index].price}
+          />
+        </div>
       </div>
-
-    ))
-  }
-
+    ));
+  };
 
   const priceInputGenerator = () => {
     let lists = [];
     for (let index = 0; index < pricingCount; index++) {
-      
       lists.push(
         <div key={index} className="d-flex">
           <div className="form-group">
@@ -249,12 +270,11 @@ const handleEditPricingChange = ({target})=>{
     return lists;
   };
 
-  const addPriceFunc = e => {
+  const addPriceFunc = (e) => {
     e.preventDefault();
-    setPricingCount(previousValue => (previousValue += 1));
+    setPricingCount((previousValue) => (previousValue += 1));
   };
-  
-  
+
   return (
     <div className="add-product-section">
       <div className="container">
@@ -287,65 +307,104 @@ const handleEditPricingChange = ({target})=>{
                 <div className="form-group">
                   <h3>Images</h3>
                   <div className="d-flex between">
-                  {object.images!==undefined?<span className="flex-1"><img className="img-fluid" src={object.images[0]} alt="product1"/></span>:null}
-                   <Input
-                    type="file"
-                    name="firstImage"
-                    onChange={handleFileUpload}
-                    className="flex-4"
-                  /> 
-                  
-                  
+                    {object.images !== undefined ? (
+                      <span className="flex-1">
+                        <img
+                          className="img-fluid"
+                          src={object.images[0]}
+                          alt="product1"
+                        />
+                      </span>
+                    ) : null}
+                    <Input
+                      type="file"
+                      name="firstImage"
+                      onChange={handleFileUpload}
+                      className="flex-4"
+                    />
                   </div>
                   <div className="d-flex between">
-                  {object.images!==undefined?<span className="flex-1" ><img className="img-fluid" src={object.images[1]}alt="product2"/></span>:null}
-                   <Input
-                    type="file"
-                    name="secondImage"
-                    onChange={handleFileUpload}
-                    className="flex-4"
-                  /> 
-                  
-                  
+                    {object.images !== undefined ? (
+                      <span className="flex-1">
+                        <img
+                          className="img-fluid"
+                          src={object.images[1]}
+                          alt="product2"
+                        />
+                      </span>
+                    ) : null}
+                    <Input
+                      type="file"
+                      name="secondImage"
+                      onChange={handleFileUpload}
+                      className="flex-4"
+                    />
                   </div>
                   <div className="d-flex between">
-                  {object.images!==undefined?<span className="flex-1" ><img className="img-fluid" src={object.images[2]}alt="product3"/></span>:null}
-                   <Input
-                    type="file"
-                    name="thirdImage"
-                    onChange={handleFileUpload}
-                    className="flex-4"
-                  /> 
-                  
-                  
+                    {object.images !== undefined ? (
+                      <span className="flex-1">
+                        <img
+                          className="img-fluid"
+                          src={object.images[2]}
+                          alt="product3"
+                        />
+                      </span>
+                    ) : null}
+                    <Input
+                      type="file"
+                      name="thirdImage"
+                      onChange={handleFileUpload}
+                      className="flex-4"
+                    />
                   </div>
                   <div className="d-flex between">
-                  {object.images!==undefined?<span className="flex-1"><img className="img-fluid" src={object.images[3]}alt="product4"/></span>:null}
-                   <Input
-                    type="file"
-                    name="fourthImage"
-                    onChange={handleFileUpload}
-                    className="flex-4"
-                  /> 
-
+                    {object.images !== undefined ? (
+                      <span className="flex-1">
+                        <img
+                          className="img-fluid"
+                          src={object.images[3]}
+                          alt="product4"
+                        />
+                      </span>
+                    ) : null}
+                    <Input
+                      type="file"
+                      name="fourthImage"
+                      onChange={handleFileUpload}
+                      className="flex-4"
+                    />
                   </div>
                   <div className="d-flex between">
-                  {object.images!==undefined?<span className="flex-1"><img className="img-fluid" src={object.images[4]} alt="product5"/></span>:null}
-                   <Input
-                    type="file"
-                    name="fifthImage"
-                    onChange={handleFileUpload}
-                    className="flex-4"
-                  /> 
+                    {object.images !== undefined ? (
+                      <span className="flex-1">
+                        <img
+                          className="img-fluid"
+                          src={object.images[4]}
+                          alt="product5"
+                        />
+                      </span>
+                    ) : null}
+                    <Input
+                      type="file"
+                      name="fifthImage"
+                      onChange={handleFileUpload}
+                      className="flex-4"
+                    />
                   </div>
                 </div>
               </Card>
               <Card>
                 <h3>Pricing</h3>
-                <div className="form-group">{method==="edit"?priceEditGenerator():priceInputGenerator()}</div>
-                {pricingCount<2?<button className="btn add" onClick={addPriceFunc}>
-                  Add Price
-                </button>:null}
+                <div className="form-group">
+                  {method === "edit"
+                    ? priceEditGenerator()
+                    : priceInputGenerator()}
+                </div>
+                {pricingCount < 2 ? (
+                  <button className="btn add" onClick={addPriceFunc}>
+                    Add Price
+                  </button>
+                ) : null}
               </Card>
               <Card>
                 <h3>Inventory</h3>
@@ -378,7 +437,21 @@ const handleEditPricingChange = ({target})=>{
                   />
                 </div>
               </Card>
+              <Card>
+                <h4>Brands</h4>
+                <div className="form-group">
+                  <label>select brand</label>
+                  {/* <Input type="text" placeholder="Enter Category" />
+                   */}
+                  <Select
+                    options={latestBrands}
+                    onChange={handleBrandSelectChange}
+                    name="brandId"
+                  />
+                </div>
+              </Card>
             </div>
+            <div className="col _small"></div>
           </div>
           <div className="row">
             <Button
@@ -392,12 +465,15 @@ const handleEditPricingChange = ({target})=>{
         {uploadSuccess
           ? izitoast.show({
               messageColor: "white",
-              title: method==="edit"?"Product Updated":"Product Added",
+              title: method === "edit" ? "Product Updated" : "Product Added",
               backgroundColor: "#00FF00",
               titleColor: "white",
               timeout: 5000,
-              message: method==="edit"?`Product ${productObj.name} successfully Updated`:`Product ${productObj.name} successfully Added`,
-              onClosed:()=>setSuccess(false)
+              message:
+                method === "edit"
+                  ? `Product ${productObj.name} successfully Updated`
+                  : `Product ${productObj.name} successfully Added`,
+              onClosed: () => setSuccess(false),
             })
           : updateError
           ? izitoast.show({
@@ -407,7 +483,7 @@ const handleEditPricingChange = ({target})=>{
               titleColor: "white",
               timeout: 5000,
               message: error,
-              onClosed:()=>setUpdateError(false)
+              onClosed: () => setUpdateError(false),
             })
           : null}
       </div>
