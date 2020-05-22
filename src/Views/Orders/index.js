c1b8ee3a7a30b378f2961a709d6a9b1aebfa5051
Tyav <tyav2greenz@gmail.com>
2020-05-22 11:43:27 +0100

    add pagination to Order

--- src/Views/Orders/index.js
+++ src/Views/Orders/index.js
@@ -1,23 +1,114 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import Card from "../../Components/Card";
 import Table from "../../Components/Table";
-import { fetchOrders } from "../../actions/orderAction";
+import { fetchOrder } from "../../actions/orderAction";
 import "./index.scss";
 import { useSelector, useDispatch } from "react-redux";
 import {formattedDate} from "../../utils/helperFunc";
 
 export default (props) => {
-  const {
-    orders: { orders },
-  } = useSelector((state) => state);
-  const dispatch = useDispatch();
+
+  const [orderPage, setOrderPage] = useState({
+    order: [],
+    page: 1
+  })
+  const [orders, setOrders] =  useState([])
+  const [query, setQuery] = useState({
+    limit: 100, skip: 1
+  }) 
+  const [page, setPage] = useState({
+    lower: 0,
+    upper: 0
+  })
+  const [limit, setLimit] = useState(20)
+
+  const [tot, setTotal] = useState(0)
+
+  const [nextData, setNextData] = useState(false)
+  // const {
+  //   orders: { orders },
+  // } = useSelector((state) => state);
+  // const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(fetchOrders());
+    if (nextData) {
+      fetchOrder(`limit=${query.limit}&skip=${query.skip}`)
+      .then((payload)=> {
+        // set order to state
+        let newOrder = [ ...orders, ...payload.data ];
+        setOrders(()=> newOrder);
+        setTotal(payload.total);
+        let upper = page.upper;
+        let lower =  page.lower
+        setPage(() => ({
+          upper: ++upper, lower: ++lower
+        }));
+    })
+    }
+  }, [query.skip]);
+  
+  useEffect(() => {
+    let upper = page.upper;
+    let lower =  page.lower
+    let order = orders.filter((trans, index) => {
+      return(index >= lower*limit && index < upper*limit) 
+    })
+    setOrderPage(()=>({
+      ...orderPage, order: order
+    }))
+  }, [page.upper])
+
+
+  useEffect(() => {
+    fetchOrder(`limit=${query.limit}&skip=${query.skip}`)
+    .then((payload)=> {
+      // set order to state
+        let newOrder = [ ...orders, ...payload.data ];
+        setOrders(()=> newOrder);
+        setTotal(payload.total);
+        let upper = page.upper;
+      setPage(() => ({
+       ...page, upper: ++upper
+      }));
+  
+    })
   }, []);
 
-  
+  const next = () => {
+    if ( page.upper*limit >= orders.length - 1) {
+      // make call to fetch the next set
+      if (query.skip * limit < tot) {
+        setNextData(true)
+        setQuery(()=> ({
+          ...query, skip: query.skip + 1
+        }))
+      }
+    } else {
+
+      let upper = page.upper;
+      let lower =  page.lower
+      setPage(() => ({
+        upper: ++upper, lower: ++lower
+      }));
+    }
+    setOrderPage(()=>({
+      ...orderPage, page: orderPage.page +1
+    }))
+    
+  }
+
+  const prev = () => {
+    let upper = page.upper;
+    let lower =  page.lower
+    setPage(() => ({
+      upper: --upper, lower: --lower
+    }));
+    setOrderPage(()=>({
+      ...orderPage, page: orderPage.page - 1
+    }))
+
+  }
   return (
     <div className="order-row">
       <div className="container">
@@ -75,14 +166,14 @@ export default (props) => {
               </thead>
               <tbody>
                 {orders.length > 0 ? (
-                  orders.map((item, index) => (
-                    <tr key={index}>
+                  orderPage.order.map((item, index) => (
+                    <tr key={item._id}>
                       <td className="checkbox">{index + 1}</td>
                       <td className="color-lgray">
                         {formattedDate(item.createdAt)}
                       </td>
                       <td className="color-lgray">
-                        {item.user.firstName + " " + item.user.lastName}
+                        {item.userId.firstName + " " + item.userId.lastName}
                       </td>
                       <td className="Payment">
                         {item.payment === "Paid" ? (
@@ -110,7 +201,7 @@ export default (props) => {
                           </div>
                         )}
                       </td>
-                      <td className="color-dgray">{item.actualAmount}</td>
+                      <td className="color-dgray">{item.amount/100}</td>
                     </tr>
                   ))
                 ) : (
@@ -120,6 +211,11 @@ export default (props) => {
                 )}
               </tbody>
             </Table>
+            <div>
+            <button className="paginate" onClick={prev} disabled={orderPage.page < 2}>{"<"}</button>
+              <span>{orderPage.page}</span>
+              <button className="paginate"  onClick={next} disabled={orderPage.page * limit >= tot}>{">"}</button>
+            </div>
           </div>
         </Card>
       </div>
