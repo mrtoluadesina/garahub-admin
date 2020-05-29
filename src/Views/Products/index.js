b5a16ec8b7d64f7f958bc30835644431111a4c27
Tyav <tyav2greenz@gmail.com>
2020-05-29 16:07:41 +0100

    atomic

--- src/Views/Products/index.js
+++ src/Views/Products/index.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
 import Icon from "@mdi/react";
+import useModal from 'use-react-modal'
 
 import axios from 'axios';
 
@@ -39,18 +40,37 @@ export default props => {
   const handleEdit = id => {console.log(id)}
 
   const handleDelete = async id => {
+    console.log(id)
     await axios.delete(`${process.env.REACT_APP_BASE_URL}/api/v1/product/${id}`, auth);
     window.location.reload();
   }
+
+  // use modal hooks
+  const { isOpen, openModal, closeModal, Modal } = useModal({
+    background: 'rgba(0, 0, 0, 0.5)', // sets the color of the backdrop, if nothing is set, there will be no backdrop
+    closeOnOutsideClick: true,
+    closeOnEsc: true,
+    isOpen: false,
+    // `event` has all the fields that a normal `event` would have such as `event.target.value`, etc.
+    // with the additional `portal` and `targetEl` added to it as seen in the examples below
+    onOpen: (event) => {
+      // can access: event.portal, event.targetEl, event.event, event.target, etc.
+    },
+    // `onClose` will not have an `event` unless you pass an `event` to `closePortal`
+    onClose({ targetEl, event, portal }) {},
+    // `targetEl` is the element that you either are attaching a `ref` to
+    // or that you are putting `openPortal` or `togglePortal` or `closePortal` on
+  
+    // in addition, any event handler such as onClick, onMouseOver, etc will be handled the same
+    onClick({ targetEl, event, portal }) {} 
+  })
   
   return (
-    <div className="product-row">
-      <div className="container">
-        <div className="product-header">
-          <h4 className="product">Products</h4>
-          <div className="productbtn">
-            {/* <OrderButton value="Export" /> */}
-          </div>
+    <div className='product-row'>
+      <div className='container'>
+        <div className='product-header'>
+          <h4 className='product'>Products</h4>
+          <div className='productbtn'>{/* <OrderButton value="Export" /> */}</div>
         </div>
         {/* <ul className="product-ul">
           <li className="export">Export</li>
@@ -62,8 +82,8 @@ export default props => {
             </Dropdown>
           </li>
         </ul> */}
-        <Card className="product-card">
-          <div className="all-product">
+        <Card className='product-card'>
+          <div className='all-product'>
             <ProductTab />
           </div>
           {/* <div className="filter">
@@ -76,40 +96,65 @@ export default props => {
               className="product-filterbar" />
           </div> */}
           <Table>
-              <thead className="th-color">
-                <tr>
-                  <th scope="col"></th>
-                  <th scope="col" className="product-chevron">
-                    Product
-                  </th>
-                  <th scope="col">Inventory</th>
-                  <th scope="col">Brand</th>
-                  <th scope="col">Date</th>
-                  <th scope="col"></th>
-                  <th scope="col"></th>
-                </tr>
-              </thead>
-              <tbody>
-                {products.filter(list => list.isDeleted === false).map((item, index) => (
+            <thead className='th-color'>
+              <tr>
+                <th scope='col'></th>
+                <th scope='col' className='product-chevron'>
+                  Product
+                </th>
+                <th scope='col'>Inventory</th>
+                <th scope='col'>Brand</th>
+                <th scope='col'>Date</th>
+                <th scope='col'></th>
+                <th scope='col'></th>
+              </tr>
+            </thead>
+            <tbody>
+              {products
+                .filter(list => list.isDeleted === false)
+                .map((item, index) => (
                   <tr key={index}>
-                    <td className="md-5">
-                      <div className="product-image">
-                        {item.images?<img src={item.images[0]} alt="product" />:<Icon path={item.images[0]} className="product-icon"></Icon>}
-                        
-                      </div>
+                    <td className='md-5'>
+                      <div className='product-image'>{item.images ? <img src={item.images[0]} alt='product' /> : <Icon path={item.images[0]} className='product-icon'></Icon>}</div>
                     </td>
-                    <td className="order-item"><Link to={`/dashboard/products/edit/${item._id}`}>{item.name}</Link>  </td>
+                    <td className='order-item'>
+                      <Link to={`/dashboard/products/edit/${item._id}`}>{item.name}</Link>{' '}
+                    </td>
                     <td>
-                      <span className="color-orange">{item.quantity} </span>
-                      </td>
-                    <td className="color-dgray">{item.brandName}</td>
-                    <td className="color-dgray date-md">{formattedDate(item.createdAt)}</td>
-                    <td className="crud edit" onClick={() => handleEdit(item._id)}>edit</td>
-                    <td className="crud del" onClick={() => handleDelete(item._id)}>delete</td>
+                      <span className='color-orange'>{item.quantity} </span>
+                    </td>
+                    <td className='color-dgray'>{item.brandName}</td>
+                    <td className='color-dgray date-md'>{formattedDate(item.createdAt)}</td>
+                    <td className='crud edit' onClick={() => handleEdit(item._id)}>
+                      edit
+                    </td>
+                    <td className='crud del' onClick={openModal}>
+                      delete
+                    </td>
+                    {isOpen && (
+                      <Modal>
+                        <div style={{
+                          backgroundColor: 'white',
+                          height: 200,
+
+                        }}>
+                          <p>
+                            Whatever you put here will be centered to the middle of the screen.
+                          </p>
+                          <div style={{
+                            display: 'flex',
+                            width: '100%',
+                          }}> 
+                            <button onClick={closeModal}>close</button>
+                            <button onClick={() => handleDelete(item._id)} >Yes, Delete</button>
+                          </div>
+                        </div>
+                      </Modal>
+                    )}
                   </tr>
                 ))}
-              </tbody>
-            </Table>
+            </tbody>
+          </Table>
         </Card>
       </div>
     </div>
