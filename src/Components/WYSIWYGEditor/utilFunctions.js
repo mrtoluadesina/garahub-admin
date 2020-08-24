a81dd23314da1c245ed07a9e2d3201a78175d63d
whitehox <oketegah@gmail.com>
2020-08-24 03:10:48 +0100

    create util function for text editor

---
+++ src/Components/WYSIWYGEditor/utilFunctions.js
@@ -0,0 +1,43 @@
+export function makeBold() {
+  return document.execCommand('bold', false, '');
+}
+
+export function makeItalic() {
+  return document.execCommand('italic', false, '');
+}
+
+export function strikeThrough() {
+  return document.execCommand('strikeThrough', false, '');
+}
+
+export function underLineText() {
+  return document.execCommand('underline', false, '');
+}
+
+export function justifyRight() {
+  return document.execCommand('justifyRight', false, '');
+}
+
+export function justifyLeft() {
+  return document.execCommand('justifyLeft', false, '');
+}
+
+export function justifyCenter() {
+  return document.execCommand('justifyCenter', false, '');
+}
+
+export function undoChange() {
+  return document.execCommand('undo', false, '');
+}
+
+export function redoChange() {
+  return document.execCommand('redo', false, '');
+}
+
+export function unorderedList() {
+  return document.execCommand('insertUnorderedList', false, '');
+}
+
+export function orderedList() {
+  return document.execCommand('insertOrderedList', false, '');
+}
