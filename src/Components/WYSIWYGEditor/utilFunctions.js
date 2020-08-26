b3ca3da357584c2ee72943411d0ee92616dcd345
whitehox <oketegah@gmail.com>
2020-08-26 11:36:34 +0100

    fix text editor

--- src/Components/WYSIWYGEditor/utilFunctions.js
+++ src/Components/WYSIWYGEditor/utilFunctions.js
@@ -1,43 +1,54 @@
-export function makeBold() {
+export function makeBold(e) {
+  e.preventDefault();
   return document.execCommand('bold', false, '');
 }
 
-export function makeItalic() {
+export function makeItalic(e) {
+  e.preventDefault();
   return document.execCommand('italic', false, '');
 }
 
-export function strikeThrough() {
+export function strikeThrough(e) {
+  e.preventDefault();
   return document.execCommand('strikeThrough', false, '');
 }
 
-export function underLineText() {
+export function underLineText(e) {
+  e.preventDefault();
   return document.execCommand('underline', false, '');
 }
 
-export function justifyRight() {
+export function justifyRight(e) {
+  e.preventDefault();
   return document.execCommand('justifyRight', false, '');
 }
 
-export function justifyLeft() {
+export function justifyLeft(e) {
+  e.preventDefault();
   return document.execCommand('justifyLeft', false, '');
 }
 
-export function justifyCenter() {
+export function justifyCenter(e) {
+  e.preventDefault();
   return document.execCommand('justifyCenter', false, '');
 }
 
-export function undoChange() {
+export function undoChange(e) {
+  e.preventDefault();
   return document.execCommand('undo', false, '');
 }
 
-export function redoChange() {
+export function redoChange(e) {
+  e.preventDefault();
   return document.execCommand('redo', false, '');
 }
 
-export function unorderedList() {
+export function unorderedList(e) {
+  e.preventDefault();
   return document.execCommand('insertUnorderedList', false, '');
 }
 
-export function orderedList() {
+export function orderedList(e) {
+  e.preventDefault();
   return document.execCommand('insertOrderedList', false, '');
 }
